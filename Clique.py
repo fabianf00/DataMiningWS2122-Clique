d01b59e5122be5bdf053772d8098b59b249a41d5
Fabian Frauenberger <fabian.frauenberger@me.com>
2021-11-13 16:13:54 +0100

    code for testing moved to main.py

--- Clique.py
+++ Clique.py
@@ -54,10 +54,3 @@ class Clique:
                     dense_unit = dict({f: unit})
                     one_dim_dense_units.append(dense_unit)
         return one_dim_dense_units
-
-
-if __name__ == '__main__':
-    data = np.array([[1, 2, 3, 4],
-                     [5, 7, 0, 1],
-                     [0, 0, 0.5, 10]])
-    clique = Clique(1, 0.1, True, data)
--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import numpy as np
 import Clique
 
 if __name__ == '__main__':
-    print("Hello World")
+    data = np.array([[1, 2, 3, 4],
+                     [5, 7, 0, 1],
+                     [0, 0, 0.5, 10]])
+    clique = Clique(1, 0.1, True, data)
