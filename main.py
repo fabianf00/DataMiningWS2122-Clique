253745a5b1f2c67ae82fd3b3945161a580aaf0f7
Fabian Frauenberger <fabian.frauenberger@me.com>
2021-11-15 10:44:33 +0100

    changed to lower dimensional data the check if everything is labeled right

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import random
 import time
 
 if __name__ == '__main__':
+    """
     list_of_data = []
-    for i in range(240000):
+    for i in range(3600):
         value = random.randint(-2, random.randint(0, 25))
         list_of_data.append(value)
-    data = np.array(list_of_data).reshape((60000, 4))
+    data = np.array(list_of_data).reshape((1200, 3))
+    """
+    data = np.array([[0,0],
+                     [1,1],
+                     [2,2],
+                     [3,3],
+                     [4,4],
+                     [5,5],
+                     [6,6]])
     start = time.time()
     print("Started processing")
-    clique = Clique(10, 0.01, data)
+    clique = Clique(3, 0.1, data)
     clique.process()
     print("Finished processing")
     print("Start Labeling")
@@ -20,3 +29,4 @@ if __name__ == '__main__':
 
     end = time.time()
     print(end - start, "seconds for execution")
+    print(labels_for_subspace)
